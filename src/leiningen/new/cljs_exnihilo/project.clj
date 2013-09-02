73a06db6a85653bb7da541ea8f973ae8aad8084f
John Mumm <jtfmumm@gmail.com>
2013-09-02 19:18:10 -0400

    Minor

--- src/leiningen/new/cljs_exnihilo/project.clj
+++ src/leiningen/new/cljs_exnihilo/project.clj
@@ -6,21 +6,19 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                             [org.clojure/clojurescript "0.0-1806"]
                             [compojure "1.1.5"]
-                            [lib-noir "0.6.8"]
-                            [prismatic/dommy "0.1.1"]
-                            [hiccup "1.0.4"]
-                            [ring/ring-jetty-adapter "1.2.0"]]
+                            [jayq "2.4.0"]
+                            [hiccup "1.0.4"]]
   :source-paths ["src/clj"]
   :plugins [[lein-cljsbuild "0.3.2"]
                  [lein-ring "0.8.7"]]
   :cljsbuild {
-              :builds [{
-                  :source-path ["src/cljs"]
-                  :id "main"
-                  :compiler {
+    :builds {
+             :main {
+                        :source-path "src/cljs"
+                        :compiler {
                                      :output-to "resources/public/js/cljs.js"
                                      :optimizations :simple
-                                     :pretty-print true}}]}
-  ;:main {{name}}.server
+                                     :pretty-print true}}}}
+  :main {{name}}.server
   :ring {:handler {{name}}.server/app})
 
