3a6f67f56cc325d2ebcc3eff484269d9c6a9f471
Ariaroo Efe <efeariaroo@gmail.com>
2015-02-11 22:32:51 +0100

    the old cljs build config in the project.clj seems to be deprecated so I have made the fix

--- src/leiningen/new/cljs_exnihilo/project.clj
+++ src/leiningen/new/cljs_exnihilo/project.clj
@@ -4,21 +4,21 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                            [org.clojure/clojurescript "0.0-1806"]
-                            [compojure "1.1.5"]
-                            [jayq "2.4.0"]
-                            [hiccup "1.0.4"]]
+                 [org.clojure/clojurescript "0.0-1806"]
+                 [compojure "1.1.5"]
+                 [jayq "2.4.0"]
+                 [hiccup "1.0.4"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
   :source-paths ["src/clj"]
   :plugins [[lein-cljsbuild "0.3.2"]
                  [lein-ring "0.8.7"]]
-  :cljsbuild {
-    :builds {
-             :main {
-                        :source-path "src/cljs"
-                        :compiler {
-                                     :output-to "resources/public/js/cljs.js"
-                                     :optimizations :simple
-                                     :pretty-print true}}}}
+
+  :cljsbuild
+    {:builds [{:source-paths ["src/cljs"],
+               :id "main",
+               :compiler {:optimizations :simple,
+                          :output-to "resources/public/js/cljs.js",
+                          :pretty-print true}}]}
   :main {{name}}.server
   :ring {:handler {{name}}.server/app})
 
